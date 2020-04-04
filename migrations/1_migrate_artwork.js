aef488fb1057ae4da060a25476ab83f5885755b8
Francis Li <mail@francisli.com>
2020-04-03 21:12:51 -0700

    Add truffle and ganache to package, restore package-lock.json, add Migrations to initial deployment

--- migrations/1_migrate_artwork.js
+++ migrations/1_migrate_artwork.js
@@ -1,10 +1,13 @@
+const Migrations = artifacts.require("./Migrations.sol");
 const AsyncArtwork = artifacts.require("./AsyncArtwork.sol");
 
 module.exports = async function(deployer) {
+  await deployer.deploy(Migrations)
+
   var title = "Async Art";
-  var symbol = "ASYNC";  
+  var symbol = "ASYNC";
 
   console.log("AsyncArtwork bytecode size: ", AsyncArtwork.deployedBytecode.length);
-  
-  await deployer.deploy(AsyncArtwork, title, symbol)  
+
+  await deployer.deploy(AsyncArtwork, title, symbol)
 };
