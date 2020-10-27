6cabd4dde0c1e4cb2bcddf97aca0638eb111f509
jonjon <jonjonclark@gmail.com>
2020-10-27 15:05:16 +0000

    Basic migrations

--- migrations/1_migrate_artwork.js
+++ migrations/1_migrate_artwork.js
@@ -1,20 +1,7 @@
 const Migrations = artifacts.require("./Migrations.sol");
 
-const AsyncArtwork_v2 = artifacts.require("./AsyncArtwork_v2.sol");
-
-const TokenUpgrader = artifacts.require("./TokenUpgrader.sol");
-
 module.exports = async function(deployer) {
   await deployer.deploy(Migrations)
 
-  var title = "Async Art";
-  var symbol = "ASYNC";
 
-  console.log("AsyncArtwork bytecode size: ", AsyncArtwork_v2.deployedBytecode.length);
-
-  let v1_deployed = await deployer.deploy(AsyncArtwork_v2);
-
-  let v2_deployed = await deployer.deploy(AsyncArtwork_v2);
-
-  await deployer.deploy(TokenUpgrader, v1_deployed.address);
 };
