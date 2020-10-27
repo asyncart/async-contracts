743c465affe33bca7967145b3010759d7cfb88d9
jonjon <jonjonclark@gmail.com>
2020-10-27 16:12:25 +0000

    Migration tweaking still.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,15 +2,27 @@ const Migrations = artifacts.require("./Migrations.sol");
 const AsyncArtwork_v2 = artifacts.require("./AsyncArtwork_v2.sol");
 const TokenUpgrader = artifacts.require("./TokenUpgrader.sol");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, accounts) {
   // await deployer.deploy(Migrations)
 
   var title = "Async Art";
   var symbol = "ASYNC";
 
-  console.log("AsyncArtwork bytecode size: ", AsyncArtwork_v2.deployedBytecode.length);
+  console.log(
+    "AsyncArtwork bytecode size: ",
+    AsyncArtwork_v2.deployedBytecode.length
+  );
 
-  let v2_deployed = await deployer.deploy(AsyncArtwork_v2);
+  await deployer.deploy(AsyncArtwork_v2);
+  let v2_deployed = await AsyncArtwork_v2.deployed();
 
-  await deployer.deploy(TokenUpgrader, v1_deployed.address);
+  // function initialize(string memory name, string memory symbol, uint256 initialExpectedTokenSupply, address _upgraderAddress) public initializer {
+  await v2_deployed.initialize("test async", "TA", 1000, accounts[0]);
+
+  //function whitelistTokenForCreator(address creator, uint256 masterTokenId, uint256 layerCount,
+  await v2_deployed.whitelistTokenForCreator(accounts[1], 0, 5, {
+    from: accounts[0],
+  });
+
+  // await deployer.deploy(TokenUpgrader, v1_deployed.address);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 /**
@@ -46,64 +46,81 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,//7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, //7545,            // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
     },
     goerli: {
       provider: () => {
-        console.log(process.env.MNEMONIC)
-        return new HDWalletProvider(process.env.MNEMONIC, 'https://goerli.infura.io/v3/' + process.env.INFURA_API_KEY)
+        console.log(process.env.MNEMONIC);
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY
+        );
       },
-      network_id: '5', // eslint-disable-line camelcase
+      network_id: "5", // eslint-disable-line camelcase
       gas: 4465030,
       gasPrice: 10000000000,
     },
     mainnet: {
       provider: () => {
-        console.log(process.env.MNEMONIC)
-        return new HDWalletProvider(process.env.MNEMONIC, 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY)
+        console.log(process.env.MNEMONIC);
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY
+        );
       },
-      network_id: '1', // eslint-disable-line camelcase
+      network_id: "1", // eslint-disable-line camelcase
       gas: 4465030,
       gasPrice: 10000000000,
     },
     rinkeby: {
       provider: () => {
-        console.log(process.env.MNEMONIC)
-        return new HDWalletProvider(process.env.MNEMONIC, 'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY)
+        console.log(process.env.MNEMONIC);
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY
+        );
       },
-      network_id: '4', // eslint-disable-line camelcase
+      network_id: "4", // eslint-disable-line camelcase
       gas: 7465030,
       gasPrice: 10000000000,
     },
 
+    graphTesting: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      gasPrice: 1000000000, // 1 gwei
+      //gas: "6721975",
+    },
+
     // Another network with more advanced options...
     // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+    // network_id: 3,       // Ropsten's id
+    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
 
     // Useful for private networks
     // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
 
@@ -117,23 +134,23 @@ module.exports = {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
-       optimizer: {
-         enabled: true,
-         runs: 200
-       },
-      // //  evmVersion: "byzantium"
-      }
-    }
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        // //  evmVersion: "byzantium"
+      },
+    },
   },
 
-  plugins: [
-    'truffle-plugin-verify'
-  ],
+  plugins: ["truffle-plugin-verify"],
   verify: {
-    preamble: "Copyright (C) 2020 Asynchronous Art, Inc.\nGNU General Public License v3.0\nFull notice https://github.com/asyncart/async-contracts/blob/master/LICENSE"
+    preamble:
+      "Copyright (C) 2020 Asynchronous Art, Inc.\nGNU General Public License v3.0\nFull notice https://github.com/asyncart/async-contracts/blob/master/LICENSE",
   },
   api_keys: {
-    etherscan: ''
-  }
-}
+    etherscan: "",
+  },
+};
