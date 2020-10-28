cdca8612916f228c9b58880ce4df62c8fd2cd7a9
jonjon <jonjonclark@gmail.com>
2020-10-28 09:26:35 +0000

    Migrations firing with first test actions

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,28 +1,50 @@
 const Migrations = artifacts.require("./Migrations.sol");
 const AsyncArtwork_v2 = artifacts.require("./AsyncArtwork_v2.sol");
 const TokenUpgrader = artifacts.require("./TokenUpgrader.sol");
+const { scripts, ConfigManager } = require("@openzeppelin/cli");
+const { add, push, create } = scripts;
 
-module.exports = async function(deployer, accounts) {
-  // await deployer.deploy(Migrations)
-
-  var title = "Async Art";
-  var symbol = "ASYNC";
-
-  console.log(
-    "AsyncArtwork bytecode size: ",
-    AsyncArtwork_v2.deployedBytecode.length
-  );
-
-  await deployer.deploy(AsyncArtwork_v2);
-  let v2_deployed = await AsyncArtwork_v2.deployed();
-
-  // function initialize(string memory name, string memory symbol, uint256 initialExpectedTokenSupply, address _upgraderAddress) public initializer {
-  await v2_deployed.initialize("test async", "TA", 1000, accounts[0]);
-
-  //function whitelistTokenForCreator(address creator, uint256 masterTokenId, uint256 layerCount,
-  await v2_deployed.whitelistTokenForCreator(accounts[1], 0, 5, {
-    from: accounts[0],
+async function deploy(options, accounts, argsArray, networkName) {
+  add({
+    contractsData: [{ name: "AsyncArtwork_v2", alias: "AsyncArtwork_v2" }],
   });
 
-  // await deployer.deploy(TokenUpgrader, v1_deployed.address);
+  await push({ ...options, force: true });
+
+  const asyncContract = await create({
+    ...options,
+    contractAlias: "AsyncArtwork_v2",
+    methodName: "initialize",
+    methodArgs: argsArray,
+  });
+
+  if (networkName == "graphTesting") {
+    const asyncInstance = await AsyncArtwork_v2.at(asyncContract.address);
+
+    // function whitelistTokenForCreator(address creator, uint256 masterTokenId, uint256 layerCount,
+    //   uint256 platformFirstSalePercentage, uint256 platformSecondSalePercentage) external onlyPlatform {
+    await asyncInstance.whitelistTokenForCreator(accounts[1], 1, 5, 10, 10, {
+      from: accounts[0],
+    });
+  }
+}
+
+module.exports = function(deployer, networkName, accounts) {
+  deployer.then(async () => {
+    // Don't try to deploy/migrate the contracts for tests
+    if (networkName === "test") {
+      return;
+    }
+    let title = "Async Art";
+    let symbol = "ASYNC";
+
+    // function initialize(string memory name, string memory symbol, uint256 initialExpectedTokenSupply, address _upgraderAddress) public initializer {
+    let argsArray = [title, symbol, 1, accounts[0]];
+
+    const { network, txParams } = await ConfigManager.initNetworkConfiguration({
+      network: networkName,
+      from: accounts[0],
+    });
+    await deploy({ network, txParams }, accounts, argsArray, networkName);
+  });
 };
